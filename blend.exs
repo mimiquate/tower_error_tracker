67770d7edb04ea1d4c70fd4b2eab081dc13293a4
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-11-26 13:14:01 -0300

    build(deps): bump error_tracker from 0.4.0 to 0.5.0

--- blend.exs
+++ blend.exs
@@ -1,3 +1,4 @@
 %{
+  error_tracker_0_4: [{:error_tracker, "~> 0.4.0"}],
   error_tracker_0_3: [{:error_tracker, "~> 0.3.0"}]
 }
--- test/support/test_app.ex
+++ test/support/test_app.ex
@@ -1,10 +1,21 @@
 defmodule TestApp.Repo.Migrations.AddErrorTracker do
   use Ecto.Migration
 
-  def up, do: ErrorTracker.Migration.up(version: 3)
+  def up, do: ErrorTracker.Migration.up(version: migration_version())
 
   # We specify `version: 1` in `down`, to ensure we remove all migrations.
   def down, do: ErrorTracker.Migration.down(version: 1)
+
+  defp migration_version do
+    Application.spec(:error_tracker, :vsn)
+    |> to_string()
+    |> Version.parse!()
+    |> Version.match?(">= 0.5.0")
+    |> case do
+      true -> 4
+      false -> 3
+    end
+  end
 end
 
 defmodule TestApp.Repo do
