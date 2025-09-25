8d42b066eab25ed3522b67c8ec39ad6086e24cc5
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-09-25 16:10:17 -0300

    test: avoids noisy IO output in tests

--- test/mix/tasks/tower_error_tracker.install_test.exs
+++ test/mix/tasks/tower_error_tracker.install_test.exs
@@ -2,26 +2,32 @@
 if Code.ensure_loaded?(Tower.Igniter) do
   defmodule Mix.Tasks.TowerErrorTracker.InstallTest do
     use ExUnit.Case, async: true
+
+    import ExUnit.CaptureIO, only: [capture_io: 1]
     import Igniter.Test
 
     test "generates everything from scratch" do
-      test_project()
-      |> Igniter.compose_task("tower_error_tracker.install", [])
-      |> assert_creates(
-        "config/config.exs",
-        """
-        import Config
-        config :tower, reporters: [TowerErrorTracker]
-        """
-      )
+      capture_io(fn ->
+        test_project()
+        |> Igniter.compose_task("tower_error_tracker.install", [])
+        |> assert_creates(
+          "config/config.exs",
+          """
+          import Config
+          config :tower, reporters: [TowerErrorTracker]
+          """
+        )
+      end)
     end
 
     test "is idempotent" do
-      test_project()
-      |> Igniter.compose_task("tower_error_tracker.install", [])
-      |> apply_igniter!()
-      |> Igniter.compose_task("tower_error_tracker.install", [])
-      |> assert_unchanged()
+      capture_io(fn ->
+        test_project()
+        |> Igniter.compose_task("tower_error_tracker.install", [])
+        |> apply_igniter!()
+        |> Igniter.compose_task("tower_error_tracker.install", [])
+        |> assert_unchanged()
+      end)
     end
   end
 end
